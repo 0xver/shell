8a06b4fb7c8b8cdf6c337b7d2272efaf18d89438
Sam Larsen <sam@slarsen.io>
2022-10-19 16:49:21 -0400

    ✨ feat: signer

--- boilerplate/execs/modules/deployer.js
+++ boilerplate/execs/modules/deployer.js
@@ -1,9 +1,14 @@
-module.exports = async function deployer(name) {
+module.exports = async function deployer(name, signer = null) {
   const contract = name;
-  const [deployer] = await ethers.getSigners();
+  var deployer;
+  if (signer === null) {
+    [deployer] = await ethers.getSigners();
+  } else {
+    deployer = signer;
+  }
   console.log(`${contract} deployer : ${deployer.address} (EOA)`);
   const token = await ethers.getContractFactory(contract);
-  const Token = await token.deploy();
+  const Token = await token.connect(deployer).deploy();
   console.log(`${contract} deployed : ${Token.address} (Contract)`);
   return Token;
 };
