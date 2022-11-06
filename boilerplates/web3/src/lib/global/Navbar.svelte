64acf64a9b9e0c98635580090f0adc761d545020
Sam Larsen <sam@slarsen.io>
2022-11-05 22:14:51 -0400

    👷‍♂️ Change header to navbar

---
+++ boilerplates/web3/src/lib/global/Navbar.svelte
@@ -0,0 +1,33 @@
+<script>
+	import logo from "$lib/images/logo.svg";
+	import Connect from "$lib/components/Connect.svelte";
+</script>
+
+<header>
+	<div class="nav">
+		<!-- svelte-ignore security-anchor-rel-noreferrer -->
+		<a href="https://github.com/0xver/shell" target="_blank">
+			<img src={logo} alt="GitHub" />
+		</a>
+	</div>
+	<div class="nav">
+		<Connect />
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.nav {
+		padding: 20px;
+	}
+
+	.nav img {
+		width: 2.7em;
+		height: 2.7em;
+		object-fit: contain;
+	}
+</style>
