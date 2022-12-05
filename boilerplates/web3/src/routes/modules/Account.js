ddba63ddcc999932219398ca752b0a467b53bb7e
Sam Larsen <sam@slarsen.io>
2022-12-04 22:46:29 -0500

    ✨ feat: Format account address

--- boilerplates/web3/src/routes/modules/Account.js
+++ boilerplates/web3/src/routes/modules/Account.js
@@ -2,9 +2,17 @@ import { provider } from "./Provider.js";
 import { ethers } from "ethers";
 import { writable } from "svelte/store";
 
-export const account = writable();
+export const accountAddress = writable();
 
-export async function getAccount() {
-	let accounts = await provider().request({ method: "eth_accounts" });
-	account.set(ethers.utils.getAddress(accounts[0]));
+export function formatAccountAddress(address) {
+	return ethers.utils.getAddress(address);
+}
+
+export async function getAccountAddress() {
+	if (provider() !== undefined) {
+		let accounts = await provider().request({ method: "eth_accounts" });
+		if (accounts.length !== 0) {
+			accountAddress.set(formatAccountAddress(accounts[0]));
+		}
+	}
 }
