ae31bd5acd342809d99471f2740f769b89a992fb
Sam Larsen <sam@slarsen.io>
2022-12-06 03:27:17 -0500

    ✨ feat: provider and windowEthereum

--- boilerplates/web3/src/routes/modules/Provider.js
+++ boilerplates/web3/src/routes/modules/Provider.js
@@ -1,4 +1,14 @@
-export function provider() {
+import { ethers } from "ethers";
+
+function getProvider() {
+	if (windowEthereum() !== undefined) {
+		return new ethers.providers.Web3Provider(windowEthereum());
+	}
+}
+
+export const provider = getProvider();
+
+export function windowEthereum() {
 	if (typeof window !== "undefined") {
 		if (typeof window.ethereum !== "undefined") {
 			return window.ethereum;
