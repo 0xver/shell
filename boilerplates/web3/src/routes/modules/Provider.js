ddbf1776abb634f915344e2dbb0aeae89510c641
Sam Larsen <sam@slarsen.io>
2022-12-07 00:16:29 -0500

    👷‍♂️ Remove ethers provider

--- boilerplates/web3/src/routes/modules/Provider.js
+++ boilerplates/web3/src/routes/modules/Provider.js
@@ -1,13 +1,3 @@
-import { ethers } from "ethers";
-
-function getProvider() {
-	if (windowEthereum() !== undefined) {
-		return new ethers.providers.Web3Provider(windowEthereum());
-	}
-}
-
-export const provider = getProvider();
-
 export function windowEthereum() {
 	if (typeof window !== "undefined") {
 		if (typeof window.ethereum !== "undefined") {
